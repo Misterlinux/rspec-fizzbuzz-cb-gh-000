77947c9a98902da5660d38a8f426ebbcf6fb7a08
Angelo zarate <mrlzarate8@gmail.com>
2018-10-11 09:43:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
-def fizzbuzz()
-
+def fizzbuzz(int)
+  if int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elsif int % 15 == 0
+    puts "fizzBuzz"
+  else
+    nil
 end
 
 # Don't forget! This file needs to be 'required' in its spec file
