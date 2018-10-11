b217b10ea1cd3f7e156556657b7a5ecba0bbdd2e
Angelo zarate <mrlzarate8@gmail.com>
2018-10-11 09:50:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-  if int % 3 = 0
-    puts "Fizz"
-  elsif int % 5 = 0
-    puts "Buzz"
-  elsif int % 15 = 0
-    puts "fizzBuzz"
+  if int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 15 == 0
+    "fizzBuzz"
   else
     nil
   end
