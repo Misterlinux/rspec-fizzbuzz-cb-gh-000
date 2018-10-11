6bcfc272648f30408d9f65adf4da7fd91bfc16d1
Angelo zarate <mrlzarate8@gmail.com>
2018-10-11 10:04:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(int)
   if ( int % 3 && int != 15) == 0
     "Fizz"
-  elsif ( int % 5 int != 15)== 0
+  elsif ( int % 5 && int != 15)== 0
     "Buzz"
   elsif int % 15 == 0
     "FizzBuzz"
